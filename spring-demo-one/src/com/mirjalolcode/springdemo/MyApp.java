1e40c1c746942b4c1509557596fd77e9e744a627
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-30 20:22:43 +0100

    Spring Inversion of Control process is being performed

--- spring-demo-one/src/com/mirjalolcode/springdemo/MyApp.java
+++ spring-demo-one/src/com/mirjalolcode/springdemo/MyApp.java
@@ -1,12 +1,20 @@
 package com.mirjalolcode.springdemo;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp {
 
 	public static void main(String[] args) {
 		// create the object
-		Coach theCoach=new TrackCoach();
+		//Coach theCoach=new TrackCoach();
 
 		// use the object
-		System.out.println(theCoach.getDailyWorkout());
+		//System.out.println(theCoach.getDailyWorkout());
+		ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext
+				("applicationContext.xml");
+		System.out.println("config loaded");
+		//BaseballCoach theCoach=(BaseballCoach)context.getBean("myCoach");
+		//theCoach.getDailyWorkout();
+		Coach theCoach=context.getBean("myCoach", Coach.class);
+		theCoach.getDailyWorkout();
 	}
 }
