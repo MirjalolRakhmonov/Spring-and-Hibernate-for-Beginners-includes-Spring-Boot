de372efd1bad8967d467e3f6ed0fbf4be59c4bbd
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-30 22:58:47 +0100

    dependency injection in Spring config file is configured

--- spring-demo-one/src/com/mirjalolcode/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/mirjalolcode/springdemo/TrackCoach.java
@@ -1,15 +1,24 @@
 package com.mirjalolcode.springdemo;
 
 public class TrackCoach implements Coach {
+	private FortuneService fortuneService;
 
 	@Override
 	public String getDailyWorkout() {
 		return "Run a hard 5k";
 	}
 
+	public TrackCoach() {
+		
+	}
+	
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
 	@Override
 	public String getDailyFortune() {
-		return null;
+		return "Just do it : " + fortuneService.getFortune();
 	}
 
 }
