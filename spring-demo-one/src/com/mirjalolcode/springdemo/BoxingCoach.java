de4ffe298d5e9249c1826db00cd85306697094d9
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-10-30 21:40:53 +0100

    Practice Activity #1 - Inversion of Control with XML Configuration is done

---
+++ spring-demo-one/src/com/mirjalolcode/springdemo/BoxingCoach.java
@@ -0,0 +1,10 @@
+package com.mirjalolcode.springdemo;
+
+public class BoxingCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Do push-ups 5 times a day";
+	}
+
+}
