e4f8cea17ac719ad10448d9e0de6f5e511e20f02
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-11-01 21:47:03 +0100

    Some work is done for Bean Life Cycle

--- spring-demo-one/src/com/mirjalolcode/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/mirjalolcode/springdemo/TrackCoach.java
@@ -20,5 +20,14 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just do it : " + fortuneService.getFortune();
 	}
-
+	
+	// add init method
+	public void startUpStaff() {
+		System.out.println("TrackCoach: inside method startUpStaff");
+	}
+	
+	// add destroy method
+	public void startUpBuilder() {
+		System.out.println("TrackCoach: inside method startUpBuilder");
+	}
 }
