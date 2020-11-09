79f7d2423f6a3dc7dc609c8fe810e245aefb3f8a
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-11-09 19:13:12 +0100

    Injecting Values from Properties file

--- spring-demo-annotations/src/com/mirjalolcode/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/mirjalolcode/springdemo/SportConfig.java
@@ -3,16 +3,18 @@ package com.mirjalolcode.springdemo;
 import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("com.mirjalolcode.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
-	// define bean for our sad service
+	// define bean f or our sad service
 	@Bean 
 	public FortuneService sadFortuneService() {
 		return new SadFortuneService();
 	}
-	
+	 
 	// define bean for swim coach AND inject dependency
 	@Bean
 	public Coach swimCoach() {
--- spring-demo-annotations/src/com/mirjalolcode/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/com/mirjalolcode/springdemo/SwimCoach.java
@@ -1,8 +1,14 @@
 package com.mirjalolcode.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 	private FortuneService fortuneService;
+	@Value("${foo.email}")
+	private String email;
 	
+	@Value("${foo.team}")
+	private String team;
 
 	public SwimCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
@@ -18,4 +24,13 @@ public class SwimCoach implements Coach {
 		return fortuneService.getFortune();
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+	
+	
 }
