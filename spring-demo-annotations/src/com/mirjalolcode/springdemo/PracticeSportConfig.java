d00bc1564047ad01410369b09f899c95d902ebf2
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-11-09 20:00:37 +0100

    Practice Activity #7 - Inversion of Control and Dependency Injection with Java Code

---
+++ spring-demo-annotations/src/com/mirjalolcode/springdemo/PracticeSportConfig.java
@@ -0,0 +1,19 @@
+package com.mirjalolcode.springdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PracticeSportConfig {
+	
+	@Bean
+	public FortuneService happyFortuneService( ) {
+		return new HappyFortuneService();
+	}
+	
+	@Bean
+	public Coach wrestlingCoach() {
+		WrestlingCoach myWrestlingCoach=new WrestlingCoach(happyFortuneService());
+		return myWrestlingCoach;
+	}
+}
