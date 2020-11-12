6a3e3e118ab9e5ab3fc9b001c44fff0ba6a8bea1
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-11-12 23:22:31 +0100

    Creating Drop-Down Lists in a slightly different way

--- spring-mvc-demo/src/com/mirjalolcode/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/mirjalolcode/springdemo/mvc/Student.java
@@ -1,13 +1,24 @@
 package com.mirjalolcode.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 	private String firstName;
 	private String lastName;
 	
 	private String country;
 	
+	private LinkedHashMap<String, String> countryOptions;
+	
 	public Student() {
+		// populate country options: used ISO country code
+		countryOptions=new LinkedHashMap<>();
 		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("ITA", "Italy");
+		countryOptions.put("UZB", "Uzbekistan");
 	}
 
 	public String getFirstName() {
@@ -33,5 +44,11 @@ public class Student {
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+	
+	
 	
 }
