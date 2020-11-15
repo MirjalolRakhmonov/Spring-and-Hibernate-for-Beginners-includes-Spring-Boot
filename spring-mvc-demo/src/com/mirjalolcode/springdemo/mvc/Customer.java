ef41e2f4c57c6ba2b385a6bf584202857494883b
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-11-15 13:40:59 +0100

    Regular expressions (regexp) are applied to Customer

--- spring-mvc-demo/src/com/mirjalolcode/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/mirjalolcode/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.mirjalolcode.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -16,6 +17,15 @@ public class Customer {
 	@Max(value = 10, message = "must be less than or equal to 10")
 	private int freePasses;
 	
+	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+	private String postalCode;
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
 	public int getFreePasses() {
 		return freePasses;
 	}
