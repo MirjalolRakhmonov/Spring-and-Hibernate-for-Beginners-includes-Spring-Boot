16c6ddc137636065aadece75403cd09882b2be6c
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-11-18 01:50:25 +0100

    Reading Objects with Hibernate

---
+++ hibernate-tutorial/src/com/mirjalolcode/hibernate/demo/ReadStudentDemo.java
@@ -0,0 +1,64 @@
+package com.mirjalolcode.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mirjalolcode.hibernate.demo.entity.Student;
+
+public class ReadStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory=new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		// create a session
+		Session session=factory.getCurrentSession();
+		
+		try {
+			// use the session object to save Java object
+			
+			// create a student object
+			System.out.println("Creating new student object...");
+			Student tempStudent=new Student("Alex", "Hunter", "alexhunter@yahoo.com");
+			
+			// start a transaction
+			session.beginTransaction();
+			 
+			// save the student object
+			System.out.println("Saving the Student...");
+			System.out.println(tempStudent);
+			session.save(tempStudent);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			// find out student's id: primary key
+			System.out.println("Saved student. Generated id: "+tempStudent.getId());
+			
+			// now get a new session and start transaction
+			session=factory.getCurrentSession();
+			session.beginTransaction();
+			
+			// retrieve student based on the id: primary key
+			System.out.println("\nGetting student with id: "+ tempStudent.getId());
+			
+			Student myStudent=session.get(Student.class, tempStudent.getId());
+			
+			System.out.println("Get complete: "+myStudent);
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
