0e23e1dc1042e7c491d9e33c61d17be16628b335
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-11-19 20:33:36 +0100

    Querying Objects with Hibernate

---
+++ hibernate-tutorial/src/com/mirjalolcode/hibernate/demo/QueryStudentDemo.java
@@ -0,0 +1,57 @@
+package com.mirjalolcode.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mirjalolcode.hibernate.demo.entity.Student;
+
+public class QueryStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory=new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		// create a session
+		Session session=factory.getCurrentSession();
+		
+		try {
+			// start a transaction
+			session.beginTransaction();
+			
+			// query students
+			List<Student> theStudents=session.createQuery("from Student").getResultList();
+			
+			// display the students
+			displayStudents(theStudents);
+			
+			// query students: last name='Rakhmonov'
+			theStudents=session.createQuery("from Student s where s.lastName='Rakhmonov'").getResultList();
+			
+			// display the students
+			System.out.println("\n\nStudents have last name of Rakhmonov");
+			displayStudents(theStudents);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+	private static void displayStudents(List<Student> theStudents) {
+		for(Student temStudent: theStudents) {
+			System.out.println(temStudent);
+		}
+	}
+
+}
