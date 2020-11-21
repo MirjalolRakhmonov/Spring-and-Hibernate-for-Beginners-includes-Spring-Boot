11f64fc669eecfb26b9d926cbb48500c3c0cb31e
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-11-21 17:23:44 +0100

    @OneToOne mapping, MainApp (CreateDemo class) is being created

--- hb-01-one-to-one-uni/src/com/mirjalolcode/hibernate/demo/CreateDemo.java
+++ hb-01-one-to-one-uni/src/com/mirjalolcode/hibernate/demo/CreateDemo.java
@@ -4,35 +4,39 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.mirjalolcode.hibernate.demo.entity.Instructor;
+import com.mirjalolcode.hibernate.demo.entity.InstructorDetail;
 import com.mirjalolcode.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class CreateDemo {
 
 	public static void main(String[] args) {
 		
 		// create session factory
 		SessionFactory factory=new Configuration()
 				.configure("hibernate.cfg.xml")
-				.addAnnotatedClass(Student.class)
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
 				.buildSessionFactory();
 		
 		// create a session
 		Session session=factory.getCurrentSession();
 		
 		try {
-			// use the session object to save Java object
+			// create the objects
+			System.out.println("Creating the objects...");
 			
-			// create a student object
-			System.out.println("Creating new student object...");
-			Student tempStudent=new Student("Mirjalol", "Rakhmonov", "mirjalolrakhmonov@yahoo.com");
+			Instructor theInstructor=new Instructor("Mirjalol", "Rakhmonov", "mirjalolrakhmonov@yahoo.com");
+			InstructorDetail tempInstructorDetail=new InstructorDetail("https://www.youtube.com","How not to code");
+			
+			// associate the objects
+			theInstructor.setInstructorDetail(tempInstructorDetail);
 			
 			// start a transaction
 			session.beginTransaction();
-			 
-			// save the student object
-			System.out.println("Saving the Student...");
-			session.save(tempStudent);
 			
+			// save the instructor
+			 
 			// commit transaction
 			session.getTransaction().commit();
 			
