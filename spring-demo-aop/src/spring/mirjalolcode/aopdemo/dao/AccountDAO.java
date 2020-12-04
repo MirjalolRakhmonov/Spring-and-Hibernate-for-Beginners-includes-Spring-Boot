76ed73d94b62d7243d4b25646e3fa1cc421c6966
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-12-04 23:48:27 +0100

    AOP: Pointcut Expressions - Match Method with Account

--- spring-demo-aop/src/spring/mirjalolcode/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop/src/spring/mirjalolcode/aopdemo/dao/AccountDAO.java
@@ -2,10 +2,12 @@ package spring.mirjalolcode.aopdemo.dao;
 
 import org.springframework.stereotype.Component;
 
+import com.mirjalolcode.aopdemo.Account;
+
 @Component
 public class AccountDAO {
 
-	public void addAccount() {
+	public void addAccount(Account theAccount) {
 		
 		System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
 		
