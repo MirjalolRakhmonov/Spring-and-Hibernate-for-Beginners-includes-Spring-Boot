f595eb4da908e513f424ef34145d137fce1b966d
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-12-08 18:12:35 +0100

    Constructors are added to Account, findAccount() to AccountDAO

--- spring-demo-aop-z-after-returning/src/spring/mirjalolcode/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop-z-after-returning/src/spring/mirjalolcode/aopdemo/dao/AccountDAO.java
@@ -1,5 +1,8 @@
 package spring.mirjalolcode.aopdemo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.mirjalolcode.aopdemo.Account;
@@ -9,6 +12,22 @@ public class AccountDAO {
 	
 	private String name;
 	private String serviceCode;
+	
+	public List<Account> findAccounts() {
+		List<Account>myAccounts=new ArrayList<>();
+		
+		// create sample accounts
+		Account temp1=new Account("John", "Platinum");
+		Account temp2=new Account("Hitler", "Silver");
+		Account temp3=new Account("George", "Gold");
+		
+		// add them to accounts list
+		myAccounts.add(temp1);
+		myAccounts.add(temp2);
+		myAccounts.add(temp3);
+		
+		return myAccounts;
+	}
 
 	public void addAccount(Account theAccount, boolean vipFlag) {
 		
