62f11f0476a53603744b2ee668bcc61e83269d0d
Mirjalol Rakhmonov <manhattan701@gmail.com>
2020-12-09 12:50:28 +0100

    @Around Advice--Prep work is done, TrafficFortuneService is created

---
+++ spring-demo-aop-z-around/src/com/mirjalolcode/aopdemo/service/TrafficFortuneService.java
@@ -0,0 +1,20 @@
+package com.mirjalolcode.aopdemo.service;
+
+import java.util.concurrent.TimeUnit;
+
+public class TrafficFortuneService {
+
+	public String getFortune() {
+		// simulate a delay
+		try {
+			TimeUnit.SECONDS.sleep(5);
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// return a fortune
+		return "Expected heavy traffic";
+		
+	}
+}
